e48b6e99539a0f50148898fc50863267559e3d85
HollowDude <madafakn@gmail.com>
2025-01-25 11:17:48 -0500

    last2

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -1,9 +1,23 @@
 extends Node2D
+var pantalla : TextureRect
+var game 
+var decay = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	game = get_child(0)
+	pantalla = get_child(1).get_child(1).get_child(0)
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click") and GlobalVariables.dead :
+		GlobalVariables.reload()
+		TerrainAdapter.reload()
+		get_tree().reload_current_scene()
+	elif event.is_action_pressed("click") and GlobalVariables.paused :
+		game.process_mode = PROCESS_MODE_INHERIT
+		GlobalVariables.pause(false)
+		get_child(1).get_child(1).get_child(2).play("a")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
