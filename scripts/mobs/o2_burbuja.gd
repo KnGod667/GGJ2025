b34ef2dbcd4f7f70b60d52cdb6b8bb6bc573b4af
6r66nin <roningarrido6@gmail.com>
2025-01-24 21:54:32 -0500

    burbuja_o2

--- scripts/mobs/o2_burbuja.gd
+++ scripts/mobs/o2_burbuja.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 func _ready() -> void:
 	
+	#cambiar variable respecto a la inclinacion
 	if position.x < 500:
 		apply_impulse(Vector2(randf_range(230, 290),-80),Vector2.ZERO)
 	else:
