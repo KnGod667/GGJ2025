98761402494ec34edcee80264e573f1767094864
KnGod667 <kngod666@gmail.com>
2025-01-25 10:12:03 -0500

    a few changes

--- scripts/personaje.gd
+++ scripts/personaje.gd
@@ -3,13 +3,10 @@ extends CharacterBody2D
 var SPEED = 5
 #Vars para el escaneo de colision, aunque las veo algo toscas
 var x = 20
-var radio = [Vector2(x, 0),Vector2(-x, 0), 
-			Vector2(0, x), Vector2(0, -x), 
-			Vector2(x, x), Vector2(-x, x), 
-			Vector2(-x, -x), Vector2(x, -x)]
 
 func _physics_process(delta: float) -> void:
-	check_Radio(global_position)
+	if TerrainAdapter.check_area_collision(global_position,x):
+		dead()
 	if !$Burbuja_player.alive:
 		look_at(get_global_mouse_position())
 		if global_position.distance_to(get_global_mouse_position()) > 5:
@@ -20,11 +17,3 @@ func _physics_process(delta: float) -> void:
 					
 func dead():
 	queue_free() # Fin del Juego
-
-#func para escanear el radio
-func check_Radio(position : Vector2):
-	for v in radio:
-		if TerrainAdapter.is_coliding(position + v):
-			dead()
-		 
-	
