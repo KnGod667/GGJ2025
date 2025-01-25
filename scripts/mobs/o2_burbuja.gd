4c6c908e0653142834f15abbcc7effe89dfd204e
6r66nin <roningarrido6@gmail.com>
2025-01-24 21:28:05 -0500

    burbuja_o2

---
+++ scripts/mobs/o2_burbuja.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+
+func _ready() -> void:
+	
+	if position.x < 500:
+		apply_impulse(Vector2(randf_range(230, 290),-80),Vector2.ZERO)
+	else:
+		apply_impulse(Vector2(randf_range(-290, -230),-80),Vector2.ZERO)
+
+		
+func _process(delta: float) -> void: 
+	
+	if position.y > 680:
+		delete_()
+	if gravity_scale < 1:
+		gravity_scale += 0.3 * delta
+
+func delete_():
+	queue_free()
+	
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # hit_ funcion
